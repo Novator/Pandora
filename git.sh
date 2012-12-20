18c6ac9aceed38cb4126ca8e88f3fa7b6690ac19
Michael Galyuk <robux@mail.ru>
2012-12-21 01:37:03 +0600

    version 0.1 alfa

--- git.sh
+++ git.sh
@@ -13,15 +13,19 @@
 
 ## Push files from local to repository
 #git add --all
-git commit -a -m "sixth commit"
-git push -u origin master
+git commit -a -m "version 0.1 alfa"
+#git push -u origin master
 
-## Make ignore list
+## Ignore list. Exclude or remove(!) files
 #nano .gitignore
 #git rm --cached rubyfull.exe
+#git rm rubyfull.exe
 
 ## Rename file or directory
 #git mv ruby.exe rubyfull.exe
 
 ## Getting repository to local directory
 #git clone https://github.com/Novator/Pandora.git
+
+## Getting changes from repository to local
+#git pull
