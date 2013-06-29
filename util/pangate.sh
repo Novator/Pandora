c717096040ffd466e247e7cce2d1ddabca8f1183
Michael Galyuk <robux@mail.ru>
2013-06-29 11:19:14 +0600

    version 0.1 alfa

---
+++ util/pangate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#This is a script for running Pandora, installing Ruby, and setting environment on Linux
+#(c) Michael Galyuk, Pandora, free software
+
+DIRFILE=`readlink -e "$0"`
+CURFILE=`basename "$DIRFILE"`
+CURDIR=`dirname "$DIRFILE"`
+
+cd $CURDIR
+
+/usr/bin/python ./pangate.py
+
