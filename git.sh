55996441aaeac578b976c9859c7e398a91eb18ba
Michael Galyuk <ironsoft@mail.ru>
2013-10-12 02:23:15 +0600

    version 0.2 alfa

--- git.sh
+++ git.sh
@@ -33,7 +33,7 @@
 ## Push files from local to repository
 #git add --all
 git commit -a -m "version 0.2 alfa"
-git push -u origin master
+git push -u origin develop
 
 ##Modify last commit
 #git commit --amend
