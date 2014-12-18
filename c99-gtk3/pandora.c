a5a44fc64c1d7808cc6bd4b62877d3dcdde4b288
OpenInformationTechnology <contact@privacyprotect.ws>
2014-12-18 22:17:20 +0300

    GTK3 pack_start & Gtk::Box

---
+++ c99-gtk3/pandora.c
@@ -0,0 +1,7 @@
+// P2P national network Pandora
+// RU: P2P народная сеть Пандора
+//
+// This program is distributed under the GNU GPLv2
+// RU: Эта программа распространяется под GNU GPLv2
+// 2012 (c) Michael Galyuk
+// RU: 2012 (c) Михаил Галюк
