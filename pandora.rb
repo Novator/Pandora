533d647c41699b9a2d7184990ca5fe1111ff32f6
Michael Galyuk <ironsoft@mail.ru>
2015-02-15 22:57:09 +0500

    version 0.3 alpha

--- pandora.rb
+++ pandora.rb
@@ -14261,12 +14261,12 @@ def delete_psocket
   File.delete(PANDORA_USOCK) if File.exist?(PANDORA_USOCK)
 end
 
-$win32api = false
+$win32api = nil
 
 # Initialize win32 unit
 # RU: Инициализирует модуль win32
 def init_win32api
-  if not $win32api
+  if $win32api.nil?
     begin
       require 'Win32API'
       $win32api = true
@@ -14369,11 +14369,169 @@ if PandoraUtils.os_family=='windows'
   $stderr = $stdout
 end
 
+$reg_init = false
+
+# Read Windows HKLM registry value by path and key
+# RU: Читает значение HKLM реестра винды по пути и ключу
+def read_win_hklm_reg(path, key)
+  res = nil
+  if $reg_init.nil?
+    begin
+      require 'win32/registry'
+      $reg_init = true
+    rescue Exception
+      $reg_init = false
+      puts 'Cannot init win32/registry'
+    end
+  end
+  if $reg_init
+    reg_type = Win32::Registry::KEY_READ | 0x100
+    Win32::Registry::HKEY_LOCAL_MACHINE.open(path, reg_type) do |reg|
+      begin
+        res = reg[key]
+      rescue
+        res = nil
+      end
+    end
+  end
+  res
+end
+
 # Get language from environment parameters
 # RU: Взять язык из переменных окружения
 lang = ENV['LANG']
 if (lang.is_a? String) and (lang.size>1)
   $lang = lang[0, 2].downcase
+elsif PandoraUtils.os_family=='windows'
+  lang_code = read_win_hklm_reg('System\CurrentControlSet\Control\Nls\Language', \
+    'InstallLanguage')
+  if lang_code
+    lcode = {}
+    lcode[0x0436] = 'af;Afrikaans'
+    lcode[0x041C] = 'sq;Albanian'
+    lcode[0x0001] = 'ar;Arabic'
+    lcode[0x0401] = 'ar-sa;Arabic (Saudi Arabia)'
+    lcode[0x0801] = 'ar-iq;Arabic (Iraq)'
+    lcode[0x0C01] = 'ar-eg;Arabic (Egypt)'
+    lcode[0x1001] = 'ar-ly;Arabic (Libya)'
+    lcode[0x1401] = 'ar-dz;Arabic (Algeria)'
+    lcode[0x1801] = 'ar-ma;Arabic (Morocco)'
+    lcode[0x1C01] = 'ar-tn;Arabic (Tunisia)'
+    lcode[0x2001] = 'ar-om;Arabic (Oman)'
+    lcode[0x2401] = 'ar-ye;Arabic (Yemen)'
+    lcode[0x2801] = 'ar-sy;Arabic (Syria)'
+    lcode[0x2C01] = 'ar-jo;Arabic (Jordan)'
+    lcode[0x3001] = 'ar-lb;Arabic (Lebanon)'
+    lcode[0x3401] = 'ar-kw;Arabic (Kuwait)'
+    lcode[0x3801] = 'ar-ae;Arabic (you.A.E.)'
+    lcode[0x3C01] = 'ar-bh;Arabic (Bahrain)'
+    lcode[0x4001] = 'ar-qa;Arabic (Qatar)'
+    lcode[0x042D] = 'eu;Basque'
+    lcode[0x0402] = 'bg;Bulgarian'
+    lcode[0x0423] = 'be;Belarusian'
+    lcode[0x0403] = 'ca;Catalan'
+    lcode[0x0004] = 'zh;Chinese'
+    lcode[0x0404] = 'zh-tw;Chinese (Taiwan)'
+    lcode[0x0804] = 'zh-cn;Chinese (China)'
+    lcode[0x0C04] = 'zh-hk;Chinese (Hong Kong SAR)'
+    lcode[0x1004] = 'zh-sg;Chinese (Singapore)'
+    lcode[0x041A] = 'hr;Croatian'
+    lcode[0x0405] = 'cs;Czech'
+    lcode[0x0406] = 'the;Danish'
+    lcode[0x0413] = 'nl;Dutch (Netherlands)'
+    lcode[0x0813] = 'nl-be;Dutch (Belgium)'
+    lcode[0x0009] = 'en;English'
+    lcode[0x0409] = 'en-us;English (United States)'
+    lcode[0x0809] = 'en-gb;English (United Kingdom)'
+    lcode[0x0C09] = 'en-au;English (Australia)'
+    lcode[0x1009] = 'en-ca;English (Canada)'
+    lcode[0x1409] = 'en-nz;English (New Zealand)'
+    lcode[0x1809] = 'en-ie;English (Ireland)'
+    lcode[0x1C09] = 'en-za;English (South Africa)'
+    lcode[0x2009] = 'en-jm;English (Jamaica)'
+    lcode[0x2809] = 'en-bz;English (Belize)'
+    lcode[0x2C09] = 'en-tt;English (Trinidad)'
+    lcode[0x0425] = 'et;Estonian'
+    lcode[0x0438] = 'fo;Faeroese'
+    lcode[0x0429] = 'fa;Farsi'
+    lcode[0x040B] = 'fi;Finnish'
+    lcode[0x040C] = 'fr;French (France)'
+    lcode[0x080C] = 'fr-be;French (Belgium)'
+    lcode[0x0C0C] = 'fr-ca;French (Canada)'
+    lcode[0x100C] = 'fr-ch;French (Switzerland)'
+    lcode[0x140C] = 'fr-lu;French (Luxembourg)'
+    lcode[0x043C] = 'gd;Gaelic'
+    lcode[0x0407] = 'de;German (Germany)'
+    lcode[0x0807] = 'de-ch;German (Switzerland)'
+    lcode[0x0C07] = 'de-at;German (Austria)'
+    lcode[0x1007] = 'de-lu;German (Luxembourg)'
+    lcode[0x1407] = 'de-li;German (Liechtenstein)'
+    lcode[0x0408] = 'el;Greek'
+    lcode[0x040D] = 'he;Hebrew'
+    lcode[0x0439] = 'hi;Hindi'
+    lcode[0x040E] = 'hu;Hungarian'
+    lcode[0x040F] = 'is;Icelandic'
+    lcode[0x0421] = 'in;Indonesian'
+    lcode[0x0410] = 'it;Italian (Italy)'
+    lcode[0x0810] = 'it-ch;Italian (Switzerland)'
+    lcode[0x0411] = 'ja;Japanese'
+    lcode[0x0412] = 'ko;Korean'
+    lcode[0x0426] = 'lv;Latvian'
+    lcode[0x0427] = 'lt;Lithuanian'
+    lcode[0x042F] = 'mk;FYRO Macedonian'
+    lcode[0x043E] = 'ms;Malay (Malaysia)'
+    lcode[0x043A] = 'mt;Maltese'
+    lcode[0x0414] = 'no;Norwegian (Bokmal)'
+    lcode[0x0814] = 'no;Norwegian (Nynorsk)'
+    lcode[0x0415] = 'pl;Polish'
+    lcode[0x0416] = 'pt-br;Portuguese (Brazil)'
+    lcode[0x0816] = 'pt;Portuguese (Portugal)'
+    lcode[0x0417] = 'rm;Rhaeto-Romanic'
+    lcode[0x0418] = 'ro;Romanian'
+    lcode[0x0818] = 'ro-mo;Romanian (Moldova)'
+    lcode[0x0419] = 'ru;Russian'
+    lcode[0x0819] = 'ru-mo;Russian (Moldova)'
+    lcode[0x0C1A] = 'sr;Serbian (Cyrillic)'
+    lcode[0x081A] = 'sr;Serbian (Latin)'
+    lcode[0x041B] = 'sk;Slovak'
+    lcode[0x0424] = 'sl;Slovenian'
+    lcode[0x042E] = 'sb;Sorbian'
+    lcode[0x040A] = 'es;Spanish (Traditional Sort)'
+    lcode[0x080A] = 'es-mx;Spanish (Mexico)'
+    lcode[0x0C0A] = 'es;Spanish (International Sort)'
+    lcode[0x100A] = 'es-gt;Spanish (Guatemala)'
+    lcode[0x140A] = 'es-cr;Spanish (Costa Rica)'
+    lcode[0x180A] = 'es-pa;Spanish (Panama)'
+    lcode[0x1C0A] = 'es-do;Spanish (Dominican Republic)'
+    lcode[0x200A] = 'es-ve;Spanish (Venezuela)'
+    lcode[0x240A] = 'es-co;Spanish (Colombia)'
+    lcode[0x280A] = 'es-pe;Spanish (Peru)'
+    lcode[0x2C0A] = 'es-ar;Spanish (Argentina)'
+    lcode[0x300A] = 'es-ec;Spanish (Ecuador)'
+    lcode[0x340A] = 'es-cl;Spanish (Chile)'
+    lcode[0x380A] = 'es-uy;Spanish (Uruguay)'
+    lcode[0x3C0A] = 'es-py;Spanish (Paraguay)'
+    lcode[0x400A] = 'es-bo;Spanish (Bolivia)'
+    lcode[0x440A] = 'es-sv;Spanish (El Salvador)'
+    lcode[0x480A] = 'es-hn;Spanish (Honduras)'
+    lcode[0x4C0A] = 'es-ni;Spanish (Nicaragua)'
+    lcode[0x500A] = 'es-pr;Spanish (Puerto Rico)'
+    lcode[0x0430] = 'sx;Sutu'
+    lcode[0x041D] = 'sv;Swedish'
+    lcode[0x081D] = 'sv-fi;Swedish (Finland)'
+    lcode[0x041E] = 'th;Thai'
+    lcode[0x0431] = 'ts;Tsonga'
+    lcode[0x0432] = 'tn;Tswana'
+    lcode[0x041F] = 'tr;Turkish'
+    lcode[0x0422] = 'uk;Ukrainian'
+    lcode[0x0420] = 'your;Urdu'
+    lcode[0x042A] = 'vi;Vietnamese'
+    lcode[0x0434] = 'xh;Xhosa'
+    lcode[0x043D] = 'ji;Yiddish'
+    lcode[0x0435] = 'zu;Zulu'
+    lang = lcode[lang_code]
+    $lang = lang[0, 2].downcase if (lang.is_a? String) and (lang.size>1)
+  end
 end
 #$lang = 'en'
 
