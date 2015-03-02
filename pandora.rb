8188613a258a12b986a388516afaa456abbb2f3e
Michael Galyuk <ironsoft@mail.ru>
2015-03-03 00:06:45 +0500

    version 0.3 alpha

--- pandora.rb
+++ pandora.rb
@@ -6587,12 +6587,12 @@ module PandoraNet
                             p log_mes+'FOUND fish session='+session.inspect
                             fish_baseid = session.to_base_id
                             line << fish_baseid
-                            fisher_lure = registrate_line_hook(session, *line)
-                            fish_lure = session.registrate_line_hook(self, *line)
+                            fisher_hook = registrate_line_hook(session, *line)
+                            fish_hook = session.registrate_line_hook(self, *line)
                             line_raw = PandoraUtils.rubyobj_to_pson(line)
-                            session.add_send_segment(EC_News, true, fish_lure.chr + line_raw, \
+                            session.add_send_segment(EC_News, true, fish_hook.chr + line_raw, \
                               ECC_News_Hook)
-                            add_send_segment(EC_News, true, fisher_lure.chr + line_raw, \
+                            add_send_segment(EC_News, true, fisher_hook.chr + line_raw, \
                               ECC_News_Hook)
                           end
                         else
@@ -11702,32 +11702,32 @@ module PandoraGtk
       update_btn.tooltip_text = title
       update_btn.label = title
 
-      hunted_btn = SafeCheckButton.new(_('hunted'), true)
-      hunted_btn.safe_signal_clicked do |widget|
-        update_btn.clicked
-      end
-      hunted_btn.safe_set_active(true)
+      #hunted_btn = SafeCheckButton.new(_('hunted'), true)
+      #hunted_btn.safe_signal_clicked do |widget|
+      #  update_btn.clicked
+      #end
+      #hunted_btn.safe_set_active(true)
 
-      hunters_btn = SafeCheckButton.new(_('hunters'), true)
-      hunters_btn.safe_signal_clicked do |widget|
-        update_btn.clicked
-      end
-      hunters_btn.safe_set_active(true)
+      #hunters_btn = SafeCheckButton.new(_('hunters'), true)
+      #hunters_btn.safe_signal_clicked do |widget|
+      #  update_btn.clicked
+      #end
+      #hunters_btn.safe_set_active(true)
 
-      fishers_btn = SafeCheckButton.new(_('fishers'), true)
-      fishers_btn.safe_signal_clicked do |widget|
-        update_btn.clicked
-      end
-      fishers_btn.safe_set_active(true)
+      #fishers_btn = SafeCheckButton.new(_('fishers'), true)
+      #fishers_btn.safe_signal_clicked do |widget|
+      #  update_btn.clicked
+      #end
+      #fishers_btn.safe_set_active(true)
 
       title = _('Delete')
       delete_btn = Gtk::ToolButton.new(Gtk::Stock::DELETE, title)
       delete_btn.tooltip_text = title
       delete_btn.label = title
 
-      hbox.pack_start(hunted_btn, false, true, 0)
-      hbox.pack_start(hunters_btn, false, true, 0)
-      hbox.pack_start(fishers_btn, false, true, 0)
+      #hbox.pack_start(hunted_btn, false, true, 0)
+      #hbox.pack_start(hunters_btn, false, true, 0)
+      #hbox.pack_start(fishers_btn, false, true, 0)
       hbox.pack_start(update_btn, false, true, 0)
       hbox.pack_start(delete_btn, false, true, 0)
 
@@ -11735,33 +11735,23 @@ module PandoraGtk
       list_sw.shadow_type = Gtk::SHADOW_ETCHED_IN
       list_sw.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC)
 
-      list_store = Gtk::ListStore.new(String, String, String, String, Integer, Integer, \
-        Integer, Integer, Integer)
+      #fish_ind, session, fisher, fisher_key, fisher_baseid, fish, fish_key, time]
+
+      list_store = Gtk::ListStore.new(Integer, Integer, String, String, String, String, \
+        String, String)
+
       update_btn.signal_connect('clicked') do |*args|
         list_store.clear
-        $window.pool.sessions.each do |session|
-          hunter = ((session.conn_mode & PandoraNet::CM_Hunter)>0)
-          if ((hunted_btn.active? and (not hunter)) \
-          or (hunters_btn.active? and hunter) \
-          or (fishers_btn.active? and session.donor))
-            sess_iter = list_store.append
-            sess_iter[0] = $window.pool.sessions.index(session).to_s
-            sess_iter[1] = session.host_ip.to_s
-            sess_iter[2] = session.port.to_s
-            sess_iter[3] = PandoraUtils.bytes_to_hex(session.node_panhash)
-            sess_iter[4] = session.conn_mode
-            sess_iter[5] = session.conn_state
-            sess_iter[6] = session.stage
-            sess_iter[7] = session.read_state
-            sess_iter[8] = session.send_state
-          end
-
-          #:host_name, :host_ip, :port, :proto, :node, :conn_mode, :conn_state,
-          #:stage, :dialog, :send_thread, :read_thread, :socket, :read_state, :send_state,
-          #:donor, :fisher_lure, :fish_lure, :send_models, :recv_models, :sindex,
-          #:read_queue, :send_queue, :confirm_queue, :params, :rcmd, :rcode, :rdata,
-          #:scmd, :scode, :sbuf, :log_mes, :skey, :rkey, :s_encode, :r_encode, :media_send,
-          #:node_id, :node_panhash, :entered_captcha, :captcha_sw, :fishes, :fishers
+        $window.pool.fish_orders.each do |fo|
+          sess_iter = list_store.append
+          sess_iter[0] = fo[0]
+          sess_iter[1] = fo[1].object_id
+          sess_iter[2] = PandoraUtils.bytes_to_hex(fo[2])
+          sess_iter[3] = PandoraUtils.bytes_to_hex(fo[3])
+          sess_iter[4] = PandoraUtils.bytes_to_hex(fo[4])
+          sess_iter[5] = PandoraUtils.bytes_to_hex(fo[5])
+          sess_iter[6] = PandoraUtils.bytes_to_hex(fo[6])
+          sess_iter[7] = PandoraUtils.time_to_str(fo[7])
         end
       end
 
@@ -11770,49 +11760,46 @@ module PandoraGtk
       #list_tree.rules_hint = true
       #list_tree.search_column = CL_Name
 
+      #fish_ind, session, fisher, fisher_key, fisher_baseid, fish, fish_key, time]
+
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new('№', renderer, 'text' => 0)
+      column = Gtk::TreeViewColumn.new('Index', renderer, 'text' => 0)
       column.set_sort_column_id(0)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('Ip'), renderer, 'text' => 1)
+      column = Gtk::TreeViewColumn.new(_('Session'), renderer, 'text' => 1)
       column.set_sort_column_id(1)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('Port'), renderer, 'text' => 2)
+      column = Gtk::TreeViewColumn.new(_('Fisher'), renderer, 'text' => 2)
       column.set_sort_column_id(2)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('Node'), renderer, 'text' => 3)
+      column = Gtk::TreeViewColumn.new(_('Fisher key'), renderer, 'text' => 3)
       column.set_sort_column_id(3)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('conn_mode'), renderer, 'text' => 4)
+      column = Gtk::TreeViewColumn.new(_('Fisher BaseID'), renderer, 'text' => 4)
       column.set_sort_column_id(4)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('conn_state'), renderer, 'text' => 5)
+      column = Gtk::TreeViewColumn.new(_('Fish'), renderer, 'text' => 5)
       column.set_sort_column_id(5)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('stage'), renderer, 'text' => 6)
+      column = Gtk::TreeViewColumn.new(_('Fish key'), renderer, 'text' => 6)
       column.set_sort_column_id(6)
       list_tree.append_column(column)
 
       renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('read_state'), renderer, 'text' => 7)
+      column = Gtk::TreeViewColumn.new(_('Time'), renderer, 'text' => 7)
       column.set_sort_column_id(7)
-      list_tree.append_column(column)
-
-      renderer = Gtk::CellRendererText.new
-      column = Gtk::TreeViewColumn.new(_('send_state'), renderer, 'text' => 8)
-      column.set_sort_column_id(8)
       list_tree.append_column(column)
 
       list_tree.signal_connect('row_activated') do |tree_view, path, column|
