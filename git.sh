a70c3988cfb28c84ff853398484bd769fee09467
Michael Galyuk <ironsoft@mail.ru>
2015-12-20 18:47:11 +0500

    version 0.44 alpha

--- git.sh
+++ git.sh
@@ -33,7 +33,7 @@
 
 ## Push files from local to repository
 #git add --all
-git commit -a -m "version 0.43 alpha"
+git commit -a -m "version 0.44 alpha"
 git push -u bitbuc master
 git push -u github master
 #git push -u bitbuc develop
