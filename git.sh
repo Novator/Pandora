55463e8a65e1feadce62ca3fb2aba560714b4353
Michael Galyuk <robux@mail.ru>
2016-04-04 15:42:48 +0500

    version 0.52 alpha

--- git.sh
+++ git.sh
@@ -46,11 +46,11 @@
 ## Push files from local to repository
 #git add --all
 git commit -a -m "version 0.52 alpha"
-#git push -u bitbuc master
-#git push -u github master
+git push -u bitbuc master
+git push -u github master
 #git push -u robux master
-git push -u bitbuc develop
-git push -u github develop
+#git push -u bitbuc develop
+#git push -u github develop
 
 ##Cancel last commit with save current files
 #git reset --soft HEAD~1
