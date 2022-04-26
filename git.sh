dff055efeffaf1ed0ba23ac4b0a6845c4e0ca56e
Michael Galyuk <ironsoft@mail.ru>
2022-04-26 19:46:06 +0500

    version 0.75 alpha

--- git.sh
+++ git.sh
@@ -52,13 +52,15 @@
 #git log --graph --decorate --oneline
 
 ##Save version, md5 and date to history log
-echo "`./pandora.sh --version` `./pandora.sh --md5`  `date +"%d.%m.%Y %T"`" >> ./doc/versions.txt
+#echo "`./pandora.sh --version` `./pandora.sh --md5`  `date +"%d.%m.%Y %T"`" >> ./doc/versions.txt
 
 ## Push files from local to repository
 #git add --all
 git commit -a -m "version 0.75 alpha"
-git push -u github main
-git push -u bitbuc main
+#git push -u github master
+git push https://`cat ./github_token.txt`@github.com/Novator/Pandora.git
+#git push -u bitbuc master
+git push https://robux:`cat ./bitbuc_token.txt`@bitbucket.org/robux/pandora.git
 #git push -u github develop
 #git push -u bitbuc develop
 #git push -u robux master
